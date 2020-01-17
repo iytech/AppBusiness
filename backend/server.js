d8d9b32e36427f1a921abaa4d967fa1541ad41c6
Yunusa <iytechx@gmail.com>
2020-01-17 00:29:32 -0800

    Add files via upload

---
+++ backend/server.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const config = require('config');
+const cors = require('cors');
+
+const app = express();
+app.use(express.json());
+app.use(cors());
+
+const port = process.env.PORT || 3000;
+
+const uri = config.get('ATLAS_URI');
+mongoose.connect(uri, { useNewUrlParser: true, useCreateIndex: true, useUnifiedTopology: true});
+
+const connection = mongoose.connection;
+
+connection.once('open', () => {
+    console.log("Your connection to MongoDB has been established successfully..");
+});
+
+app.listen(port, () => {
+    console.log(`Your Server is running on port: ${port}`);
+});
